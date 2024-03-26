a94264dbde6af6e44da244c238ca1887324ef1ce
Nikita Noskov <noskovnikita@lenta.ru>
2024-03-26 18:17:39 +0300

    addRoman

--- src/Main.java
+++ src/Main.java
@@ -5,7 +5,7 @@ class Main
     public static void main(String[] args)
     {
         Scanner scanner = new Scanner(System.in);
-        System.out.println("Введите математические выражение. Например, 30 + 47: ");
+        System.out.println("Введите математические выражение. Например, 30 + 47 или III + VII: ");
         String input = scanner.nextLine();
 
         try
@@ -29,9 +29,17 @@ class Main
             throw new Exception("Не соответствует требованиям - два операнда и один оператор!");
         }
 
-        double number1 = Double.parseDouble(parts[0]);
+        boolean isRoman = isRomanNumeral(parts[0]) && isRomanNumeral(parts[2]);
+        boolean isArabic = !isRoman;
+
+        if (isArabic && isRoman)
+        {
+            throw new Exception("Числа должны быть либо только арабскими, либо только римскими!");
+        }
+
+        double number1 = isRoman ? romanToArabic(parts[0]) : Double.parseDouble(parts[0]);
         String operator = parts[1];
-        double number2 = Double.parseDouble(parts[2]);
+        double number2 = isRoman ? romanToArabic(parts[2]) : Double.parseDouble(parts[2]);
 
         int result = 0;
 
@@ -68,6 +76,91 @@ class Main
             default:
                 throw new Exception("Неверно введен оператор вычисления!");
         }
-        return String.valueOf(result);
+        return isRoman ? arabicToRoman(result) : String.valueOf(result);
+    }
+
+    private static boolean isRomanNumeral(String s)
+    {
+        return s.matches("^(?=[MDCLXVI])M*(C[MD]|D?C{0,3})(X[CL]|L?X{0,3})(I[XV]|V?I{0,3})$");
+    }
+
+    private static int romanToArabic(String roman)
+    {
+        int result = 0;
+
+        switch(roman)
+        {
+            case "I":
+                result = 1;
+                break;
+            case "II":
+                result = 2;
+                break;
+            case "III":
+                result = 3;
+                break;
+            case "IV":
+                result = 4;
+                break;
+            case "V":
+                result = 5;
+                break;
+            case "VI":
+                result = 6;
+                break;
+            case "VII":
+                result = 7;
+                break;
+            case "VIII":
+                result = 8;
+                break;
+            case "IX":
+                result = 9;
+                break;
+            case "X":
+                result = 10;
+                break;
+        }
+        return result;
+    }
+
+    private static String arabicToRoman(int arabic)
+    {
+        String result = null;
+
+        switch(arabic)
+        {
+            case 1:
+                result = "I";
+                break;
+            case 2:
+                result = "II";
+                break;
+            case 3:
+                result = "III";
+                break;
+            case 4:
+                result = "IV";
+                break;
+            case 5:
+                result = "V";
+                break;
+            case 6:
+                result = "VI";
+                break;
+            case 7:
+                result = "VII";
+                break;
+            case 8:
+                result = "VIII";
+                break;
+            case 9:
+                result = "IX";
+                break;
+            case 10:
+                result = "X";
+                break;
+        }
+        return result;
     }
 }
